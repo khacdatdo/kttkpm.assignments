559721e56f9eb465cbe6ba38ce34353be42101ea
Đỗ Khắc Đạt <khacdatdo@gmail.com>
2023-03-03 19:19:38 +0700

    3-2

---
+++ assignment-3/3-2/microservice/discoveryserver/src/main/java/com/example/discoveryserver/DiscoveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserverApplication.class, args);
+	}
+
+}
---
+++ assignment-3/3-2/microservice/profilemanagement/src/main/java/com/example/profilemanagement/ProfilemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.profilemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilemanagementApplication.class, args);
+	}
+
+}
